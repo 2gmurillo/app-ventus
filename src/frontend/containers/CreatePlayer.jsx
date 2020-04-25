4ca96a8709f9d62463aa8fb40b452c58654d616b
2gmurillo <2gmurillo@gmail.com>
2020-04-25 17:27:45 -0500

    Faltan los tags

--- src/frontend/containers/CreatePlayer.jsx
+++ src/frontend/containers/CreatePlayer.jsx
@@ -2,20 +2,7 @@ import React from 'react';
 import { withFormik, Field, Form } from 'formik';
 
 const CreatePlayer = (props) => {
-  const { isSubmitting } = props;
-
-  const handleInput = (e) => {
-    setValues({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleTags = (e, i) => {
-    const { tags } = form;
-    tags[i] = e.target.value;
-    setValues({ ...form, tags });
-  };
+  const { isSubmitting, values } = props;
 
   return (
     <section className='createPlayer'>
@@ -27,18 +14,21 @@ const CreatePlayer = (props) => {
             name='cover'
             type='text'
             placeholder='Imagen'
+            value={values.cover}
           />
           <Field
             className='input'
             name='name'
             type='text'
             placeholder='Nombre'
+            value={values.name}
           />
           <Field
             className='input'
             name='position'
             type='text'
             placeholder='Posición'
+            value={values.position}
           />
           <Field
             className='input'
@@ -52,44 +42,57 @@ const CreatePlayer = (props) => {
             name='saque'
             type='number'
             placeholder='Saque'
+            value={values.saque}
           />
           <Field
             className='input'
             name='rece'
             type='number'
             placeholder='Rece'
+            value={values.rece}
           />
           <Field
             className='input'
             name='levante'
             type='number'
             placeholder='Levante'
+            value={values.levante}
           />
           <Field
             className='input'
             name='ataque'
             type='number'
             placeholder='Ataque'
+            value={values.ataque}
           />
           <Field
             className='input'
             name='bloque'
             type='number'
             placeholder='Bloque'
+            value={values.bloque}
           />
           <Field
             className='input'
             name='defensa'
             type='number'
             placeholder='Defensa'
+            value={values.defensa}
           />
           <Field
             className='input'
             name='category'
             type='text'
             placeholder='Categoría'
+            value={values.tags[1]}
           />
-          <Field className='input' name='sexo' type='text' placeholder='Sexo' />
+          <Field
+            className='input'
+            name='sexo'
+            type='text'
+            placeholder='Sexo'
+            value={values.tags[0]}
+          />
           <button disabled={isSubmitting} type='submit' className='button'>
             Create
           </button>
@@ -115,6 +118,11 @@ export default withFormik({
       tags: [],
     };
   },
+  // handleTags(e, i) {
+  //   const { tags } = form;
+  //   tags[i] = e.target.value;
+  //   setValues({ ...form, tags });
+  // },
   handleSubmit(values, formikBag) {
     console.log(values);
     formikBag.isSubmitting(false);
