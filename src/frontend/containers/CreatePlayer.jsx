7761683febc6888712f7fcc5ac41498de03ae10a
2gmurillo <2gmurillo@gmail.com>
2020-04-25 16:06:39 -0500

    firstCommit: Intentando con formik

--- src/frontend/containers/CreatePlayer.jsx
+++ src/frontend/containers/CreatePlayer.jsx
@@ -1,22 +1,8 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createPlayerAction } from '../actions/index';
+import React from 'react';
+import { withFormik, Field, Form } from 'formik';
 
 const CreatePlayer = (props) => {
-  const [form, setValues] = useState({
-    cover: '',
-    name: '',
-    position: '',
-    height: 0,
-    saque: 0,
-    rece: 0,
-    levante: 0,
-    ataque: 0,
-    bloque: 0,
-    defensa: 0,
-    tags: [],
-  });
+  const { isSubmitting } = props;
 
   const handleInput = (e) => {
     setValues({
@@ -31,161 +17,106 @@ const CreatePlayer = (props) => {
     setValues({ ...form, tags });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.createPlayerAction(form, '/');
-  };
-
   return (
     <section className='createPlayer'>
       <section className='createPlayer__container'>
         <h1>Create a player</h1>
-        <form className='createPlayer__container--form' onSubmit={handleSubmit}>
-          <input
+        <Form className='createPlayer__container--form'>
+          <Field
             className='input'
             name='cover'
             type='text'
             placeholder='Imagen'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='name'
             type='text'
             placeholder='Nombre'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='position'
             type='text'
             placeholder='Posición'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='height'
             type='number'
             step='any'
-            placeholder='Estatura'
-            onChange={handleInput}
             required
           />
-          <input
+          <Field
             className='input'
             name='saque'
             type='number'
             placeholder='Saque'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='rece'
             type='number'
             placeholder='Rece'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='levante'
             type='number'
             placeholder='Levante'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='ataque'
             type='number'
             placeholder='Ataque'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='bloque'
             type='number'
             placeholder='Bloque'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='defensa'
             type='number'
             placeholder='Defensa'
-            onChange={handleInput}
-            required
           />
-          <input
+          <Field
             className='input'
             name='category'
             type='text'
             placeholder='Categoría'
-            onChange={(event) => handleTags(event, 1)}
-            required
           />
-          <input
-            className='input'
-            name='sexo'
-            type='text'
-            placeholder='Sexo'
-            onChange={(event) => handleTags(event, 0)}
-            required
-          />
-          <button type='submit' className='button'>
+          <Field className='input' name='sexo' type='text' placeholder='Sexo' />
+          <button disabled={isSubmitting} type='submit' className='button'>
             Create
           </button>
-        </form>
+        </Form>
       </section>
     </section>
   );
 };
 
-const mapDispatchToProps = {
-  createPlayerAction,
-};
-
-// CreatePlayer.propTypes = {
-//   form: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       cover: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       position: PropTypes.string.isRequired,
-//       height: PropTypes.number.isRequired,
-//       saque: PropTypes.number.isRequired,
-//       rece: PropTypes.number.isRequired,
-//       levante: PropTypes.number.isRequired,
-//       ataque: PropTypes.number.isRequired,
-//       bloque: PropTypes.number.isRequired,
-//       defensa: PropTypes.number.isRequired,
-//       tags: PropTypes.array.isRequired,
-//     }).isRequired).isRequired,
-//     createPlayerAction: PropTypes.func,
-// };
-
-// CreatePlayer.propTypes = {
-//   cover: PropTypes.string.isRequired,
-//   name: PropTypes.string.isRequired,
-//   position: PropTypes.string.isRequired,
-//   height: PropTypes.number.isRequired,
-//   saque: PropTypes.number.isRequired,
-//   rece: PropTypes.number.isRequired,
-//   levante: PropTypes.number.isRequired,
-//   ataque: PropTypes.number.isRequired,
-//   bloque: PropTypes.number.isRequired,
-//   defensa: PropTypes.number.isRequired,
-//   tags: PropTypes.array.isRequired,
-//   createPlayerAction: PropTypes.func,
-// };
-
-CreatePlayer.propTypes = {
-  createPlayerAction: PropTypes.func,
-};
-
-export default connect(null, mapDispatchToProps)(CreatePlayer);
+export default withFormik({
+  mapPropsToValues(props) {
+    return {
+      cover: '',
+      name: '',
+      position: '',
+      height: 0,
+      saque: 0,
+      rece: 0,
+      levante: 0,
+      ataque: 0,
+      bloque: 0,
+      defensa: 0,
+      tags: [],
+    };
+  },
+  handleSubmit(values, formikBag) {
+    console.log(values);
+    formikBag.isSubmitting(false);
+  },
+})(CreatePlayer);
