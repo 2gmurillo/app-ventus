ec45a1d4fadac5b119f91373005aaf3171d0df37
2gmurillo <2gmurillo@gmail.com>
2020-04-27 14:36:31 -0500

    Funciona el despliegue. Probando los css

--- src/server/server.js
+++ src/server/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 // Basic strategy
 require('./utils/auth/strategies/basic');
