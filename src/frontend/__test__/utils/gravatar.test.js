1e44b67a67aadcdd2be923fc8ee7b876929244d5
2gmurillo <2gmurillo@gmail.com>
2020-04-27 14:23:03 -0500

    Fusion success in development

--- src/frontend/__test__/utils/gravatar.test.js
+++
@@ -1,8 +0,0 @@
-import gravatar from '../../utils/gravatar';
-
-test('Gravatar Function test', () => {
-  const email = '2gmurillo@gmail.com';
-  const gravatarUrl =
-    'https://gravatar.com/avatar/7ca45fe6ed52f751c88df3f9bfa34da6';
-  expect(gravatarUrl).toEqual(gravatar(email));
-});
